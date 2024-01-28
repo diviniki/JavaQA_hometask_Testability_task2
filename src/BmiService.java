40fc561657d238239df9b9bb6a331420864f6c87
dmitriyn <d.nikitin.rf@gmail.com>
2024-01-28 19:47:08 +0500

    recoding and rename files

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,15 +1,14 @@
 public class BmiService {
-    public int calculate(int a, int b) {
+    public int calculate(int weightKg, int heightCm) {
         double indexBmi;
         double heightM;
         double heightMSq;
-        heightM =  (double)b / (double)100;
-        heightMSq =  heightM*heightM;
-        indexBmi = (double)a / heightMSq;
-        return (int)indexBmi;
+        heightM = (double) heightCm / (double) 100;
+        heightMSq = heightM * heightM;
+        indexBmi = (double) weightKg / heightMSq;
+        return (int) indexBmi;
 
     }
-
 
 
 }
